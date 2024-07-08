90344db74dfe421927e7d3cce7d3ba7bbbd7bd9d
David Leyland <david@plain.com>
2024-07-08 15:10:48 +0100

    Thread page and filter by tenant

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import styles from "./page.module.css";
 import { PlainClient } from "@team-plain/typescript-sdk";
 
+export const fetchCache = "force-no-store";
+
+const apiKey = process.env.PLAIN_API_KEY;
+if (!apiKey) {
+  throw new Error("Please set the `PLAIN_API_KEY` environment variable");
+}
+
+const client = new PlainClient({
+  apiKey,
+});
+
 export default async function Home({
   params,
   searchParams,
@@ -8,22 +19,26 @@ export default async function Home({
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  console.log(searchParams);
-
-  const apiKey = process.env.PLAIN_API_KEY;
-  if (!apiKey) {
-    return (
-      <main className={styles.main}>
-        Please set the `PLAIN_API_KEY` environment variable
-      </main>
-    );
-  }
-  const client = new PlainClient({
-    apiKey,
+  const threads = await client.getThreads({
+    filters: {
+      tenantIdentifiers: [{ tenantId: "te_01J299SM3E25EJHT7JKYS6G7K5" }],
+    },
   });
-  const threads = await client.getThreads({});
 
-  console.log(threads);
+  console.log(threads.data?.threads.length);
 
-  return <main className={styles.main}>Hello</main>;
+  return (
+    <main className={styles.main}>
+      <h1>Hello</h1>
+      <div>
+        {threads.data?.threads.map((thread) => {
+          return (
+            <div key={`thread-row-${thread.id}`}>
+              {thread.title} {thread.id}
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
---
+++ app/thread/[threadId]/page.tsx
@@ -0,0 +1,74 @@
+export default async function ThreadPage({
+  params,
+}: {
+  params: { threadId: string };
+}) {
+  const data = await fetch("https://core-api.uk.plain.com/graphql/v1", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `{ 
+      
+        thread(threadId: "th_01J299WQGA3VNQ4FDECV7JK6MC") {
+            timelineEntries {
+                edges {
+                    node {
+                        id
+                        timestamp {
+                            iso8601
+                        }
+                        actor {
+                            __typename
+                            ... on UserActor {
+                                user {
+                                    fullName
+                                }
+                            }
+                            ... on CustomerActor {
+                                customer {
+                                    fullName
+                                }
+                            }
+                            ... on MachineUserActor {
+                                machineUser {
+                                    fullName
+                                }
+                            }
+                        }
+                        entry {
+                            __typename
+                            ... on CustomEntry {
+                                title
+                                components {
+                                    __typename
+                                    ... on ComponentText {
+                                    text
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+      
+      
+      }`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Plain-Workspace-Id": "w_01J28VHKDK5PV3DJSZAA01XGAN",
+      Authorization:
+        "Bearer plainApiKey_d70hpgkn58cEQMWAfj6YgiNyvl9tvHRlHfUrQb5xUnM",
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
+  return (
+    <div>
+      {params.threadId}
+      {JSON.stringify({ data })}
+    </div>
+  );
+}
