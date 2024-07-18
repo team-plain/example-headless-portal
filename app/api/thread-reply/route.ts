a75153e74f852a6120d5527934d16e0431d2aea7
David Leyland <david@plain.com>
2024-07-18 12:09:18 +0100

    lint

--- app/api/thread-reply/route.ts
+++ app/api/thread-reply/route.ts
@@ -5,18 +5,21 @@ export async function POST(request: Request) {
   // In production validation of the request body might be necessary.
   const body = await request.json();
 
-  const replyRes = await plainClient.replyToThread({textContent: body.message, threadId: body.threadId})
-    if (replyRes.error) {
-        console.error(
-            inspect(replyRes.error, {
-              showHidden: false,
-              depth: null,
-              colors: true,
-            })
-          );
-          return new Response(replyRes.error.message, { status: 500 });
-    }
+  const replyRes = await plainClient.replyToThread({
+    textContent: body.message,
+    threadId: body.threadId,
+  });
+  if (replyRes.error) {
+    console.error(
+      inspect(replyRes.error, {
+        showHidden: false,
+        depth: null,
+        colors: true,
+      })
+    );
+    return new Response(replyRes.error.message, { status: 500 });
+  }
 
-    console.log('Replied to thread', body.threadId);
-    return new Response("", { status: 200 });
+  console.log("Replied to thread", body.threadId);
+  return new Response("", { status: 200 });
 }
